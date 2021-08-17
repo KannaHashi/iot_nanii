b4c1c594727128344143d8274db56a21339e238c
CotoMKS <cn1250425@gmail.com>
2021-08-17 23:53:14 +0800

    [Pull Request] Renovating the whole index.js

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const app = express()
 const User = require('./models/User')
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://cotomks27:Chandra1250425@cluster0.dlu0f.mongodb.net/iot-shit?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useFindAndModify: true,
-  useUnifiedTopology: true
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 }, () => {
   console.log('Dah konek, tempec!')
 })
@@ -17,21 +18,30 @@ const http = require('http').createServer(app)
 app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
+// Set Kartu yang mau di register
 let CardUID = ""
-
-app.post('/add_card', (req,res) => {
+app.post('/set_card_uid', (req,res) => {
   CardUID = req.body.card_uid
   console.log(req.body)
-  return res.status(200).json({message : "Success!", content_received: req.body.card_uid})
+  return res.status(200).json({message : "Success!", card_uid: req.body.card_uid})
 })
 
+// Cek Kartu yang sedang aktif
 app.get('/card', (req,res) => {
   console.log(CardUID)
-  return res.status(200).json({current_card: CardUID})
+  return res.status(200).json({card_uid: CardUID})
 })
 
+// Registrasi kartu ke Database
+
+/* 
+  Form registrasi
+  Buka http://localhost/register_card untuk me-registrasi kartu
+*/
+
 app.get('/register_card', async(req,res) => {
-  const checkData = await User.findOne({card_uid: req.body.card_uid})
+  const checkData = await User.findOne({card_uid: CardUID})
+  console.log(checkData)
 
   let form_data = {
     card_uid: CardUID,
@@ -41,7 +51,7 @@ app.get('/register_card', async(req,res) => {
 
   if(checkData) {
     form_data = {
-      card_uid: checkData.card_uid,
+      card_uid: CardUID,
       user_name: checkData.user_name,
       coins: checkData.coins
     }
@@ -49,11 +59,12 @@ app.get('/register_card', async(req,res) => {
   res.render('register', {form_data})
 })
 
-app.post('/register', async(req,res) => {
+// Mengirim data hasil registrasi ke database
+app.post('/register_card', async(req,res) => {
   await User.findOneAndUpdate({
-    card_uid: req.body.card_uid
+    card_uid: CardUID
   }, {
-    card_uid: req.body.card_uid,
+    card_uid: CardUID,
     user_name: req.body.user_name,
     coins: 0
   }, {
@@ -61,7 +72,7 @@ app.post('/register', async(req,res) => {
     upsert: true
   }).then(() => {
     const data_sent = {
-      card_uid: req.body.card_uid,
+      card_uid: CardUID,
       user_name: req.body.user_name,
       coins: 0
     }
@@ -70,6 +81,15 @@ app.post('/register', async(req,res) => {
     console.error(e)
   })
 })
+
+/*
+API Endpoint
+
+POST http://localhost/set_card_uid --> Set kartu yang mau diregistrasi
+GET http://localhost/card --> Cek kartu yang sedang aktif (yang sudah di-set di '/set_card_uid')
+GET http://localhost/register_card --> Dibuka di Browser, isi form untuk me-registrasi kartu
+POST http://localhost/register_card --> Proses memasukkan data dari form registrasi ke database
+*/
 
 http.listen(80, () => {
   console.log('Listening to port 80')
